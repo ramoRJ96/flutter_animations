22858db6d7fc96dffc543e3f7dab9b0c7d87a5d0
ramoRJ96 <moiseraidjy@gmail.com>
2022-01-03 13:45:27 +0300

    add decorated transition in transitions list

--- lib/pages/transition_list.dart
+++ lib/pages/transition_list.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_animations/animation_demo/animation_controller.dart';
 import 'package:flutter_animations/models/material_design.dart';
 import 'package:flutter_animations/models/section.dart';
+import 'package:flutter_animations/models/transition_type.dart';
 import 'package:flutter_animations/widgets/tile.dart';
 
 class TransitionList extends StatelessWidget {
@@ -12,31 +14,31 @@ class TransitionList extends StatelessWidget {
       Section(
           name: 'DecoratedBox',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.decoratedBox)),
       Section(
           name: 'Fade',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.fade)),
       Section(
-          name: 'Positionned',
+          name: 'Positioned',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.positioned)),
       Section(
           name: 'Rotation',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.rotation)),
       Section(
           name: 'Scale',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.scale)),
       Section(
           name: 'Size',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.size)),
       Section(
           name: 'Slide',
           icon: Icons.compare_arrows,
-          destination: EmptyWidget()),
+          destination: AnimationControllerDemo(type: TransitionType.slide)),
     ];
 
     return ListView.separated(
